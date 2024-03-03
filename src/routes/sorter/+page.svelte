d70e1ca7356f2491b4d277f410b316c1bfe06830
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-03-02 23:16:47 -0500

    add results screen

--- src/routes/sorter/+page.svelte
+++ src/routes/sorter/+page.svelte
@@ -1,7 +1,7 @@
 <script lang=ts>
     import { RaceSort } from "$lib/racesort";
 	import type { Track } from "@spotify/web-api-ts-sdk";
-	import { LocalStorage, ClickableTile, Column, Grid, Row, Button } from "carbon-components-svelte";
+	import { LocalStorage, ClickableTile, Column, Grid, Row, Button, DataTable, Tooltip } from "carbon-components-svelte";
     import { TreeChart } from '@carbon/charts-svelte'
     import '@carbon/charts-svelte/styles.css'
     import type { PageData } from "./$types";
@@ -59,59 +59,72 @@
             event.dataTransfer?.setDragImage(img, 0, 0);
         }, false);
     })
+
+    $: result = (sorter.toArray() ?? []).map((song, i) => { return {
+        id: i + 1,
+        rank: i + 1,
+        name: song.name,
+        artist: song.artists[0].name,
+    };});
 </script>
 
-<h1>Sorting "{data.title}"</h1>
-<br />
+{#if currentRace.length > 0}
+    <h1>Sorting "{data.title}"</h1>
+    <br/>
+    <h3>Race {raceNumber}</h3>
+    <br/>
+    <Button on:click={() => { updateSorter(); raceNumber = raceNumber + 1;}}>
+        Run Race
+    </Button>
+    <br/><br/>
+    <p>Drag the boxes around to order them from best to worst -- a higher position corresponds to a higher rank.</p>
+    <br/><br/>
+{:else}
+    <h1>Sorted "{data.title}"</h1>
+    <br />
+{/if}
 
-<h3>Race {raceNumber}</h3>
-<br />
-
-<Button on:click={() => {
-    updateSorter(); 
-    raceNumber = raceNumber + 1;
-    }}>
-    Run Race
-</Button>
-
-<br/>
-<br/>
-
-<Column>
-    {#each currentRace as participant (participant)}
-        <div class='tile' 
-            draggable={true}
-            animate:flip={{ duration: dragDuration }}
-            on:dragstart={() => draggingCard = participant}
-            on:dragend={() => draggingCard = undefined}
-            on:dragenter={() => swapWith(participant)}
-            on:dragover|preventDefault
-            role='button' tabindex="0">
-            <ClickableTile on:click={e => {e.preventDefault();}}>
-                <Grid condensed={true} as={true}>
-                    <Row>
-                        <Column>
-                            <img src={participant.album.images[1].url} width='80px' alt='' />
-                        </Column>
-                        <Column>
-                            <h5>{participant.name}<h4>
-                            <p>{participant.artists[0].name}</p>
-                        </Column>
-                        <Column>
-                            <audio class="decrease-volume" controls style="width: 50%; height: 40px;">
-                                <source src="{participant.preview_url}" type="audio/mp3">
-                                Your browser does not support the audio element.
-                            </audio> 
-                        </Column>
-                    </Row>
-                </Grid>
-            </ClickableTile>
-        </div>
-    {/each}
-</Column>
-<br/>
-
-<!-- <h3>Sorter Graph</h3> -->
-<!-- <br /> -->
-
-<!-- <TreeChart data={sorter.getRoot().children} {options} /> -->
+{#if currentRace.length > 0}
+    <Column>
+        {#each currentRace as participant (participant)}
+            <div class='tile' 
+                draggable={true}
+                animate:flip={{ duration: dragDuration }}
+                on:dragstart={() => draggingCard = participant}
+                on:dragend={() => draggingCard = undefined}
+                on:dragenter={() => swapWith(participant)}
+                on:dragover|preventDefault
+                role='button' tabindex="0">
+                <ClickableTile on:click={e => {e.preventDefault();}}>
+                    <Grid condensed={true} as={true}>
+                        <Row>
+                            <Column>
+                                <img src={participant.album.images[1].url} width='80px' alt='' />
+                            </Column>
+                            <Column>
+                                <h5>{participant.name}<h4>
+                                <p>{participant.artists[0].name}</p>
+                            </Column>
+                            <Column>
+                                <audio class="decrease-volume" controls style="width: 50%; height: 40px;">
+                                    <source src="{participant.preview_url}" type="audio/mp3">
+                                    Your browser does not support the audio element.
+                                </audio> 
+                            </Column>
+                        </Row>
+                    </Grid>
+                </ClickableTile>
+            </div>
+        {/each}
+    </Column>
+    <br/>
+{:else}
+<DataTable 
+    headers={[
+        { key: "rank", value: "Rank" },
+        { key: "name", value: "Name" },
+        { key: "artist", value: "Artist" },
+    ]}
+    rows={result}
+/>
+{/if}
